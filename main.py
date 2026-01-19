71d00b898b908019a7ed75d9a8bf5431c4a13bf8
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-01-19 15:42:07 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#1-misol
+
+fruits = ['olma', 'banan', 'uzum']
+
+fruits.append('nok')
+print(fruits)
+
+#2-misol
+length = len(fruits)
+print("2-vazifa: Ro'yxatning uzunligi:", length)
+
+#3-misol
+
+if 'olma' in fruits:
+    print("3-vazifa: 'olma' ro'yxatda mavjud")
+else:
+    print("3-vazifa: 'olma' ro'yxatda mavjud emas")
+
+#4-misol
+
+print("4-vazifa: 'banan' ning indeksi:", fruits.index('banan'))
+
+#5-misol
+fruits = ['olma', 'banan', 'uzum']
+print(fruits)
